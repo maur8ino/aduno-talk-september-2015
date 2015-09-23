f774ca28205e4c22d77080fb590faabdf9dcacad
Mauro Verrocchio <mauroverrocchio@gmail.com>
2015-09-23 14:27:43 +0200

    adds simple test

---
+++ test/GithubSearchCtrl-test.js
@@ -0,0 +1,44 @@
+/*global angular: true, module: true, inject: true, describe: true, beforeEach: true, it: true, expect: true */
+
+describe('Controller: GithubSearchCtrl', function () {
+  'use strict';
+
+  var $rootScope, mockGithubSeachService, dummyEvent, sutCtrl, deferred;
+
+  beforeEach(module('GithubSearchModule'));
+  beforeEach(function() {
+    dummyEvent = { preventDefault: angular.noop };
+
+    inject(function($q, $timeout) {
+      mockGithubSeachService = {
+        getUserReposList: function(username) {
+          deferred = $q.defer();
+          return deferred.promise;
+        },
+        getUserRepo: angular.noop
+      };
+    });
+
+    inject(function(_$controller_, _$rootScope_) {
+      $rootScope = _$rootScope_;
+      sutCtrl = _$controller_('GithubSearchCtrl', {
+        'GithubSearchService': mockGithubSeachService
+      });
+    });
+  });
+
+  it('should have initialized the controller', function () {
+    expect(sutCtrl.loading).toBeFalsy();
+    expect(angular.isFunction(sutCtrl.submitUsername)).toBeTruthy();
+    expect(angular.isFunction(sutCtrl.submitRepo)).toBeTruthy();
+  });
+
+  it('should call submitUsername and get a $q promise', function () {
+    sutCtrl.selectedUser = 'maur8ino';
+    sutCtrl.submitUsername(dummyEvent);
+
+    deferred.resolve([]);
+    $rootScope.$digest();
+    expect(sutCtrl.loading).toBeFalsy();
+  });
+});
